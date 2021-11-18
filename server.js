f220b12fad13c95bc5a7931b419ee79c8211c3b8
guillaumeC76 <gui.colombel76@gmail.com>
2021-11-18 17:44:04 +0100

    accés

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-//global.Score = require('./api/models/scoreModel');
-//const routes = require('./api/routes/scoreRoutes');
+global.Score = require('./api/models/scoreModel');
+const routes = require('./api/routes/scoreRoutes');
 
-/*mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise;
 mongoose.connect(
   'mongodb+srv://nifoo:9tdLinTmEwKj6OOZ@cluster0.vmw6l.mongodb.net/gweb-projet',
   { useNewUrlParser: true }
-); */
+);
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-//routes(app);
+routes(app);
 app.listen(port);
 
 app.use((req, res) => {
