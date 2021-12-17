577da59e72841d27f79b9dbc477795dddf8c5dbc
guillaumeC76 <gui.colombel76@gmail.com>
2021-12-17 11:31:10 +0100

    Server.js

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+global.Score = require('./api/models/scoreModel');
+const routes = require('./api/routes/scoreRoutes');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(
+  'mongodb+srv://guillaume:cKyvJYrtpAF7ZxFs@cluster0.vmw6l.mongodb.net/gweb-projet',
+  { useNewUrlParser: true }
+);
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+routes(app);
+app.listen(port);
+
+app.use((req, res) => {
+  res.status(200).send({ url: `${req.originalUrl} ok` });
+});
+
+console.log(`Server started on port ${port}`);
